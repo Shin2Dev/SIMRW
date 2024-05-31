24096335959a9920316d4e0b31b21d24eabab1ae
Tyase Nisa'an Jamilaa <tyasenj@gmail.com>
2024-05-31 13:18:55 +0700

    Update Landing Page 3

--- laravel/resources/views/landing.blade.php
+++ laravel/resources/views/landing.blade.php
@@ -1,45 +1,249 @@
 <!DOCTYPE html>
 <html lang="en">
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
-    <link rel="stylesheet" href="assets/css/landing.css">
-    <title>Landing</title>
-</head>
+	<head>
+		<meta charset="utf-8">
+		<title>SIMRW</title>
+		<meta http-equiv="X-UA-Compatible" content="IE=Edge">
+		<meta name="viewport" content="width=device-width, initial-scale=1">
+		<meta name="keywords" content="">
+		<meta name="description" content="">
 
-<body>
+		<link rel="stylesheet" href="assets/css/animate.min.css">
+		<link rel="stylesheet" href="assets/css/bootstrap.min.css">
+		<link rel="stylesheet" href="assets/css/font-awesome.min.css">
+		<link href='//fonts.googleapis.com/css?family=Open+Sans:400,300,400italic,700,800' rel='stylesheet' type='text/css'>
+		<link rel="stylesheet" href="assets/css/landing.css">
 
-    <nav>
-        <a href="#" class="logo">SIMRW</a>
-        <div class="links">
-        </div>
+	</head>
 
-        {{-- <div class="login">
-            <a href="{{ url('/login') }}">
-                <button class="signup">Login</button>
-            </a>
-            <a href="{{ url('/register') }}">
-                <button>Daftar</button>
-            </a>
-        </div> --}}
+	<body>
+		<div class="preloader">
+			<div class="sk-spinner sk-spinner-rotating-plane"></div>
+		</div>
 
-    </nav>
+		<nav class="navbar navbar-default navbar-fixed-top templatemo-nav" role="navigation">
+			<div class="container">
+				<div class="navbar-header">
+					<button class="navbar-toggle" data-toggle="collapse" data-target=".navbar-collapse">
+						<span class="icon icon-bar"></span>
+						<span class="icon icon-bar"></span>
+						<span class="icon icon-bar"></span>
+					</button>
+					<a href="#" class="navbar-brand"><img src="assets/imgs/logo.png" alt="Logo"></a>
+				</div>
+				<div class="collapse navbar-collapse">
+					<ul class="nav navbar-nav navbar-right text-uppercase">
+						<li><a href="#home">Home</a></li>
+						<li><a href="#divider">Features</a></li>
+						<li><a href="#Feature">About Us</a></li>
+						<li><a href="#download">Login</a></li>
+						<li><a href="#contact">Contact</a></li>
+						<li><a href="#login">Login</a></li>
+					</ul>
+				</div>
+			</div>
+		</nav>
 
-    <header>
-        <div class="left">
-            <h1>Sistem Informasi <span>Rukun Warga</span></h1>
-            <p>Sistem Informasi RW 01 (SIMRW) adalah sebuah aplikasi berbasis web yang dirancang untuk membantu pengurus RW dan RT dalam mengelola data dan informasi di wilayah RW 01, serta memfasilitasi komunikasi antara warga, pengurus RW dan RT. 
-            </p>
-            <a href="{{ url('/login') }}">
-                <i class='bx bx-log-in'></i>
-                <span>Login</span>
-            </a>
-        </div>
-        <img src="assets/imgs/header.png">
-    </header>
+		<section id="home">
+			<div class="jumbotron jumbotron-fluid">
+				<div class="container">
+				  <h1 class="display-4"><span>Sistem Informasi</span><br><span>Rukun Warga</span></h1>
+				  <p>Sistem Informasi Rukun Warga (SIMRW) adalah sebuah aplikasi berbasis web yang dirancang untuk membantu pengurus
+					RW dan RT dalam mengelola data dan informasi di wilayah RW 01, serta memfasilitasi komunikasi antara warga, pengurus RW dan RT.</p>
+				  <!--<a href="#" class="btn btn-info btn-lg tombol">Login</a> -->
+				</div>
+			  </div>
+		</section>
 
-</body>
+		<section id="divider">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-4 wow fadeInUp templatemo-box" data-wow-delay="0.3s">
+						<i class="fa fa-user"></i>
+						<h3 class="text-uppercase">Data Warga</h3>
+						<p>Menu ‘Data Warga’ menampilkan pengelolaan dan dokumentasi informasi tentang penduduk di  wilayah Rukun Warga. </p>
+					</div>
+					<div class="col-md-4 wow fadeInUp templatemo-box" data-wow-delay="0.3s">
+						<i class="fa fa-envelope-o"></i>
+						<h3 class="text-uppercase">Surat</h3>
+						<p>Menu ‘Surat’  digunakan oleh warga untuk mengajukan surat yang dibutuhkan kepada ketua RT di wilayah Rukun Warga. </p>
+					</div>
+					<div class="col-md-4 wow fadeInUp templatemo-box" data-wow-delay="0.3s">
+						<i class="fa fa-hand-o-up"></i>
+						<h3 class="text-uppercase">Bantuan Sosial</h3>
+						<p>Menu ‘Bantuan Sosial’ warga yang layak diberikan bantuan sosial oleh pemerintah di wilayah Rukun Warga. </p>
+					</div>
+					<div class="col-md-4 wow fadeInUp templatemo-box" data-wow-delay="0.3s">
+						<i class="fa fa-money"></i>
+						<h3 class="text-uppercase">Keuangan</h3>
+						<p>Menu ‘Keuangan’ menampilkan pengelolaan keuangan oleh RT wilayah Rukun Warga. </p>
+					</div>
+					<div class="col-md-4 wow fadeInUp templatemo-box" data-wow-delay="0.3s">
+						<i class="fa fa-calendar-o"></i>
+						<h3 class="text-uppercase">Informasi Kegiatan</h3>
+						<p>Menu ‘Informasi Kegiatan’ menampilkan informasi kegiatan yang akan dilaksanakan di  wilayah Rukun Warga.</p>
+					</div>
+				</div>
+			</div>
+		</section>
+
+		<section id="feature">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-6 wow fadeInLeft" data-wow-delay="0.6s">
+						<h2 class="text-uppercase">ABOUT US</h2>
+						<p>Sistem Informasi Rukun Warga (SIMRW) adalah sebuah aplikasi berbasis web yang dirancang untuk membantu pengurus
+						RW dan RT dalam mengelola data dan informasi di wilayah RW 01, serta memfasilitasi komunikasi antara warga, pengurus RW dan RT.</p>
+					</div>
+					<div class="col-md-6 wow fadeInRight" data-wow-delay="0.6s">
+						<img src="assets/img/home-bg.jpg" class="img-responsive img-rounded" alt="feature img">
+					</div>
+				</div>
+			</div>
+		</section>
+
+		<!-- start pricing 
+		<section id="pricing">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-12 wow bounceIn">
+						<h2 class="text-uppercase">Our Pricing</h2>
+					</div>
+					<div class="col-md-4 wow fadeIn" data-wow-delay="0.6s">
+						<div class="pricing text-uppercase">
+							<div class="pricing-title">
+								<h4>Standard</h4>
+								<p>$4</p>
+								<small class="text-lowercase">monthly</small>
+							</div>
+							<ul>
+								<li>6 GB Space</li>
+								<li>600 GB Bandwidth</li>
+								<li>60 More Themes</li>
+								<li>Lifetime Support</li>
+							</ul>
+							<button class="btn btn-primary text-uppercase">Sign up</button>
+						</div>
+					</div>
+					<div class="col-md-4 wow fadeIn" data-wow-delay="0.6s">
+						<div class="pricing active text-uppercase">
+							<div class="pricing-title">
+								<h4>Premium</h4>
+								<p>$10</p>
+								<small class="text-lowercase">monthly</small>
+							</div>
+							<ul>
+								<li>15 GB space</li>
+								<li>1,500 GB Bandwidth</li>
+								<li>150 More Themes</li>
+								<li>Lifetime Support</li>
+							</ul>
+							<button class="btn btn-primary text-uppercase">Sign up</button>
+						</div>
+					</div>
+					<div class="col-md-4 wow fadeIn" data-wow-delay="0.6s">
+						<div class="pricing text-uppercase">
+							<div class="pricing-title">
+								<h4>Professional</h4>
+								<p>$80</p>
+								<small class="text-lowercase">monthly</small>
+							</div>
+							<ul>
+								<li>35 GB space</li>
+								<li>3,500 GB bandwidth</li>
+								<li>350 more themes</li>
+								<li>Lifetime Support</li>
+							</ul>
+							<button class="btn btn-primary text-uppercase">Sign Up</button>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section> -->
+
+		<section id="download">
+			<div class="container">
+				<div class="row">
+					<div class="col-md-6 wow fadeInLeft" data-wow-delay="0.6s">
+						<h2 class="text-uppercase">Login</h2>
+						<a href="{{ url('/login') }}" class="btn btn-primary text-uppercase">Login</a>
+					</div>
+					<div class="col-md-6 wow fadeInRight" data-wow-delay="0.6s">
+						<img src="assets/img/home-bg.jpg" class="img-responsive img-rounded" alt="feature img">
+					</div>
+				</div>
+			</div>
+		</section>
+
+	<!--	<section id="contact">
+			<div class="overlay">
+				<div class="container">
+					<div class="row">
+						<div class="col-md-6 wow fadeInUp" data-wow-delay="0.6s">
+							<h2 class="text-uppercase">Contact Us</h2>
+							<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
+								dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation. </p>
+							<address>
+								<p><i class="fa fa-map-marker"></i>123 State ST, Maine, United States</p>
+								<p><i class="fa fa-phone"></i> 123-123-1234 or 123-321-3212</p>
+								<p><i class="fa fa-envelope-o"></i> mail@company.com</p>
+							</address>
+						</div>
+						<div class="col-md-6 wow fadeInUp" data-wow-delay="0.6s">
+							<div class="contact-form">
+								<form action="#" method="post">
+									<div class="col-md-6">
+										<input type="text" class="form-control" placeholder="Name">
+									</div>
+									<div class="col-md-6">
+										<input type="email" class="form-control" placeholder="Email">
+									</div>
+									<div class="col-md-12">
+										<input type="text" class="form-control" placeholder="Subject">
+									</div>
+									<div class="col-md-12">
+										<textarea class="form-control" placeholder="Message" rows="4"></textarea>
+									</div>
+									<div class="col-md-12">
+										<input type="submit" class="form-control text-uppercase" value="Send">
+									</div>
+								</form>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section> -->
+
+		<footer>
+			<div class="container">
+				<div class="row">
+					<p>Copyright © 2024 Kelompok 7</p>
+				</div>
+			</div>
+		</footer>
+
+		<script src="assets/js/jquery.js"></script>
+		<script src="assets/js/bootstrap.min.js"></script>
+		<script src="assets/js/wow.min.js"></script>
+		<script src="assets/js/jquery.singlePageNav.min.js"></script>
+	    <script>
+			$(window).load(function () {
+				$('.preloader').fadeOut(1000);
+			});
+
+			$(function () {
+				new WOW().init();
+				$('.templatemo-nav').singlePageNav({
+					offset: 70
+				});
+
+				$('.navbar-collapse a').click(function () {
+					$(".navbar-collapse").collapse('hide');
+				});
+			})
+		</script>
+	</body>
 
 </html>
