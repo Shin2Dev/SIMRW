3172a986b06085d0f1c832be6fcc9294f7a5534c
Tyase Nisa'an Jamilaa <tyasenj@gmail.com>
2024-06-02 19:55:15 +0700

    Update Landing Page 4

--- laravel/resources/views/landing.blade.php
+++ laravel/resources/views/landing.blade.php
@@ -18,10 +18,6 @@
 	</head>
 
 	<body>
-		<div class="preloader">
-			<div class="sk-spinner sk-spinner-rotating-plane"></div>
-		</div>
-
 		<nav class="navbar navbar-default navbar-fixed-top templatemo-nav" role="navigation">
 			<div class="container">
 				<div class="navbar-header">
@@ -35,11 +31,11 @@
 				<div class="collapse navbar-collapse">
 					<ul class="nav navbar-nav navbar-right text-uppercase">
 						<li><a href="#home">Home</a></li>
-						<li><a href="#divider">Features</a></li>
-						<li><a href="#Feature">About Us</a></li>
-						<li><a href="#download">Login</a></li>
-						<li><a href="#contact">Contact</a></li>
+						<li><a href="#feature">Features</a></li>
+						<li><a href="#aboutus">About Us</a></li>
+						<li><a href="#informasi">Informasi</a></li>
 						<li><a href="#login">Login</a></li>
+						<li><a href="#contact">Contact</a></li>
 					</ul>
 				</div>
 			</div>
@@ -56,7 +52,7 @@
 			  </div>
 		</section>
 
-		<section id="divider">
+		<section id="feature">
 			<div class="container">
 				<div class="row">
 					<div class="col-md-4 wow fadeInUp templatemo-box" data-wow-delay="0.3s">
@@ -88,138 +84,114 @@
 			</div>
 		</section>
 
-		<section id="feature">
+		<section id="aboutus">
 			<div class="container">
 				<div class="row">
+					<div class="col-md-6 wow fadeInRight" data-wow-delay="0.6s">
+						<img src="assets/imgs/home-bg.jpg" class="img-responsive img-rounded" alt="feature img">
+					</div>
 					<div class="col-md-6 wow fadeInLeft" data-wow-delay="0.6s">
 						<h2 class="text-uppercase">ABOUT US</h2>
 						<p>Sistem Informasi Rukun Warga (SIMRW) adalah sebuah aplikasi berbasis web yang dirancang untuk membantu pengurus
 						RW dan RT dalam mengelola data dan informasi di wilayah RW 01, serta memfasilitasi komunikasi antara warga, pengurus RW dan RT.</p>
 					</div>
-					<div class="col-md-6 wow fadeInRight" data-wow-delay="0.6s">
-						<img src="assets/img/home-bg.jpg" class="img-responsive img-rounded" alt="feature img">
-					</div>
 				</div>
 			</div>
 		</section>
 
-		<!-- start pricing 
-		<section id="pricing">
+		<!-- start pricing -->
+		<section id="informasi">
 			<div class="container">
 				<div class="row">
 					<div class="col-md-12 wow bounceIn">
-						<h2 class="text-uppercase">Our Pricing</h2>
+						<h2 class="text-uppercase">Informasi Kegiatan</h2>
 					</div>
 					<div class="col-md-4 wow fadeIn" data-wow-delay="0.6s">
 						<div class="pricing text-uppercase">
 							<div class="pricing-title">
-								<h4>Standard</h4>
-								<p>$4</p>
-								<small class="text-lowercase">monthly</small>
+								<img src="assets/imgs/kerjabakti.jpg" alt="pricing img" class="pricing-image">
 							</div>
 							<ul>
-								<li>6 GB Space</li>
-								<li>600 GB Bandwidth</li>
-								<li>60 More Themes</li>
-								<li>Lifetime Support</li>
+								<li> Diberitahukan kepada seluruh warga RW 01 akan diakan Kerja Bakti Rutinan. Kegiatan Kerja Bakti rutinan warga akan dilaksanakan pada:</li>
+								<li> Tanggal: 12 Mei 2024 </li>
+								<li> Tempat: Halaman depan kantor desa</li>
 							</ul>
-							<button class="btn btn-primary text-uppercase">Sign up</button>
+							<button class="btn btn-primary text-uppercase">Detail</button>
 						</div>
 					</div>
 					<div class="col-md-4 wow fadeIn" data-wow-delay="0.6s">
 						<div class="pricing active text-uppercase">
 							<div class="pricing-title">
-								<h4>Premium</h4>
-								<p>$10</p>
-								<small class="text-lowercase">monthly</small>
+								<img src="assets/imgs/kerjabakti.jpg" alt="pricing img" class="pricing-image">
 							</div>
 							<ul>
-								<li>15 GB space</li>
-								<li>1,500 GB Bandwidth</li>
-								<li>150 More Themes</li>
-								<li>Lifetime Support</li>
+								<li> Diberitahukan kepada seluruh warga RW 01 akan diakan Kerja Bakti Rutinan. Kegiatan Kerja Bakti rutinan warga akan dilaksanakan pada:</li>
+								<li> Tanggal: 12 Mei 2024 </li>
+								<li> Tempat: Halaman depan kantor desa</li>
 							</ul>
-							<button class="btn btn-primary text-uppercase">Sign up</button>
+							<button class="btn btn-primary text-uppercase">Detail</button>
 						</div>
 					</div>
 					<div class="col-md-4 wow fadeIn" data-wow-delay="0.6s">
 						<div class="pricing text-uppercase">
 							<div class="pricing-title">
-								<h4>Professional</h4>
-								<p>$80</p>
-								<small class="text-lowercase">monthly</small>
+								<img src="assets/imgs/kerjabakti.jpg" alt="pricing img" class="pricing-image">
 							</div>
 							<ul>
-								<li>35 GB space</li>
-								<li>3,500 GB bandwidth</li>
-								<li>350 more themes</li>
-								<li>Lifetime Support</li>
+								<li> Diberitahukan kepada seluruh warga RW 01 akan diakan Kerja Bakti Rutinan. Kegiatan Kerja Bakti rutinan warga akan dilaksanakan pada:</li>
+								<li> Tanggal: 12 Mei 2024 </li>
+								<li> Tempat: Halaman depan kantor desa</li>
 							</ul>
-							<button class="btn btn-primary text-uppercase">Sign Up</button>
+							<button class="btn btn-primary text-uppercase">Detail</button>
 						</div>
 					</div>
 				</div>
 			</div>
-		</section> -->
+		</section> 
 
-		<section id="download">
+		<section id="login">
 			<div class="container">
 				<div class="row">
 					<div class="col-md-6 wow fadeInLeft" data-wow-delay="0.6s">
 						<h2 class="text-uppercase">Login</h2>
-						<a href="{{ url('/login') }}" class="btn btn-primary text-uppercase">Login</a>
+						<p>Silahkan login untuk melanjutkan</p>
+						<a href="https://github.com/jgmuchiri/simple-landing-page/archive/master.zip" class="btn btn-primary text-uppercase"> Login</a>
 					</div>
 					<div class="col-md-6 wow fadeInRight" data-wow-delay="0.6s">
-						<img src="assets/img/home-bg.jpg" class="img-responsive img-rounded" alt="feature img">
+						<img src="assets/imgs/gambarlogin.jpg" class="img-responsive img-rounded" alt="feature img">
 					</div>
 				</div>
 			</div>
 		</section>
 
-	<!--	<section id="contact">
+		<section id="contact">
 			<div class="overlay">
 				<div class="container">
 					<div class="row">
 						<div class="col-md-6 wow fadeInUp" data-wow-delay="0.6s">
 							<h2 class="text-uppercase">Contact Us</h2>
-							<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et
-								dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation. </p>
+							<p> Untuk informasi lebih lanjut anda bisa mengakses melalui kontak di bawah ini,
+								Kami selalu siap membantu Anda dengan segala pertanyaan atau kebutuhan yang mungkin 
+								Anda miliki. Jangan ragu untuk menghubungi kami melalui salah satu kontak berikut: </p>
 							<address>
-								<p><i class="fa fa-map-marker"></i>123 State ST, Maine, United States</p>
-								<p><i class="fa fa-phone"></i> 123-123-1234 or 123-321-3212</p>
-								<p><i class="fa fa-envelope-o"></i> mail@company.com</p>
+								<p><i class="fa fa-map-marker"></i>Tegalgondo Gondang, Kec. Karangploso</p>
+								<p><i class="fa fa-phone"></i>081230875757</p>
+								<p><i class="fa fa-envelope-o"></i>rwsatutegalgondo@gmail.com</p>
 							</address>
 						</div>
 						<div class="col-md-6 wow fadeInUp" data-wow-delay="0.6s">
 							<div class="contact-form">
-								<form action="#" method="post">
-									<div class="col-md-6">
-										<input type="text" class="form-control" placeholder="Name">
-									</div>
-									<div class="col-md-6">
-										<input type="email" class="form-control" placeholder="Email">
-									</div>
-									<div class="col-md-12">
-										<input type="text" class="form-control" placeholder="Subject">
-									</div>
-									<div class="col-md-12">
-										<textarea class="form-control" placeholder="Message" rows="4"></textarea>
-									</div>
-									<div class="col-md-12">
-										<input type="submit" class="form-control text-uppercase" value="Send">
-									</div>
-								</form>
 							</div>
 						</div>
 					</div>
 				</div>
 			</div>
-		</section> -->
+		</section> 
 
 		<footer>
 			<div class="container">
 				<div class="row">
-					<p>Copyright © 2024 Kelompok 7</p>
+					<p>Copyright © <span>Sistem Informasi Rukun Warga</span></p>
 				</div>
 			</div>
 		</footer>
@@ -228,22 +200,6 @@
 		<script src="assets/js/bootstrap.min.js"></script>
 		<script src="assets/js/wow.min.js"></script>
 		<script src="assets/js/jquery.singlePageNav.min.js"></script>
-	    <script>
-			$(window).load(function () {
-				$('.preloader').fadeOut(1000);
-			});
-
-			$(function () {
-				new WOW().init();
-				$('.templatemo-nav').singlePageNav({
-					offset: 70
-				});
-
-				$('.navbar-collapse a').click(function () {
-					$(".navbar-collapse").collapse('hide');
-				});
-			})
-		</script>
 	</body>
 
 </html>
